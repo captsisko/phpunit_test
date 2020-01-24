7999b2af9cb0a169a02a82d2628330c70e171cd9
sisko <adeoduye@gmail.com>
2020-01-24 17:30:53 +0000

    Installed Mockery

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,11 +1,20 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
 
-class ExampleTest extends TestCase
+class ExampleTest extends MockeryTestCase
 {
     public function testAdding2And2()
     {
         $this->assertEquals(4, 2 + 2);
     }
+
+    protected function tearDown(): void
+    {
+        // code
+        Mockery::close();
+    }
+    
+    
 }
