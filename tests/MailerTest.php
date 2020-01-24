ff1aaebc96bc1d976f538ddd379dfe2879b34163
sisko <adeoduye@gmail.com>
2020-01-24 11:49:35 +0000

    Now using a basic stub which returns NULL by default

--- tests/MailerTest.php
+++ tests/MailerTest.php
@@ -12,7 +12,8 @@ class MailerTest extends TestCase
     public function test_function()
     {
         // Test
-        $mailer = new Mailer();
+        // $mailer = new Mailer();
+        $mailer = $this->createMock(Mailer::class);
         $result = $mailer->sendMessage('sisko@ds9.fed', 'Hailing frequencies open');
 
         var_dump($result);
